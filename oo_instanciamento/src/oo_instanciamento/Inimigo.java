3c29e66582f7f78181eaa87cf4d946030c36aa4d
ossfx <43969228+ossfx@users.noreply.github.com>
2018-11-07 08:54:38 -0200

    ajustes inimigo
    ajustes inimigo

--- oo_instanciamento/src/oo_instanciamento/Inimigo.java
+++ oo_instanciamento/src/oo_instanciamento/Inimigo.java
@@ -2,5 +2,12 @@ package oo_instanciamento;
 
 public class Inimigo {
 	
-		
+	public void iniciarInimigo() {
+		System.out.println("Inimigo iniciado");	
+	}
+	public void adicionarVidaInimigo() {
+		System.out.println("Vida adicionada ao Inimigo");		
+	}
 }
+
+
